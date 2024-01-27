81652049a8d903dd76d35510f2475b1182e15247
Makstorm <mxm.horobets@gmail.com>
2024-01-27 18:15:51 +0200

    feat: new transaction list component

---
+++ client/src/components/Wallet/TransactionsList/index.tsx
@@ -0,0 +1,69 @@
+import { FC, useEffect } from "react";
+import "./index.scss";
+import { transactionsAPI } from "../../../store/services/TransactionsService";
+import TransactionSkeleton from "../TransactionsSceleton";
+import TransactionCard from "../TransactionCard";
+
+const TransactionsList: FC = () => {
+  const { data, isLoading, refetch } =
+    transactionsAPI.useFetchUserTransactionsQuery("");
+
+  useEffect(() => {
+    // Refetch data when the component mounts or when the page gains focus
+    refetch();
+
+    const onFocus = () => {
+      refetch();
+    };
+
+    const intervalId = setInterval(() => refetch(), 10000);
+
+    window.addEventListener("focus", onFocus);
+
+    return () => {
+      window.removeEventListener("focus", onFocus);
+      clearInterval(intervalId);
+    };
+  }, [refetch]);
+
+  if (isLoading) {
+    return (
+      <div className="transactions-list">
+        <TransactionSkeleton number={8} />
+      </div>
+    );
+  }
+
+  if (!data || data.length < 1) {
+    return (
+      <div className="transactions-list">
+        <h1>No transactions</h1>
+        {/* <TransactionCard
+          transaction={{
+            id: 3,
+            amount: 20,
+            sender: "Stripe",
+            type: TransactionType.REMPLENISHABLE,
+            receiver: "me",
+            date: new Date(),
+            category: "food",
+          }}
+        /> */}
+      </div>
+    );
+  }
+
+  return (
+    <div className="transactions-list">
+      {data.map((transaction) => {
+        console.log(transaction);
+        return (
+          <TransactionCard key={transaction.id} transaction={transaction} />
+        );
+        // return <div key={index}></div>;
+      })}
+    </div>
+  );
+};
+
+export default TransactionsList;
