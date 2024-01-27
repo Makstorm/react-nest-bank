3e28c3ed55ad0b928c193894779058ef1a727808
Makstorm <mxm.horobets@gmail.com>
2024-01-27 18:33:15 +0200

    feat: new Wallet page component

--- client/src/pages/WalletPage/index.tsx
+++ client/src/pages/WalletPage/index.tsx
@@ -1,8 +1,31 @@
-import React from "react";
 import "./index.scss";
+import PhonePage from "../../components/Phone/PhonePage";
+import PhonePageContent from "../../components/Phone/PhonePageContent";
+import WalletNavigate from "../../components/Wallet/WalletNavigate";
+import Balance from "../../components/Wallet/Balance";
+import ReceiveSend from "../../components/Wallet/ReceiveSend";
+import TransactionsList from "../../components/Wallet/TransactionsList";
 
 const WalletPage = () => {
-  return <div>WalletPage</div>;
+  return (
+    <PhonePage className="wallet-page">
+      <header className="wallet-page__header-container">
+        <section style={{ marginTop: "60px", padding: "0 20px" }}></section>
+        <PhonePageContent>
+          <WalletNavigate />
+
+          <Balance />
+
+          <ReceiveSend />
+        </PhonePageContent>
+      </header>
+      <main>
+        <PhonePageContent>
+          <TransactionsList />
+        </PhonePageContent>
+      </main>
+    </PhonePage>
+  );
 };
 
 export default WalletPage;
