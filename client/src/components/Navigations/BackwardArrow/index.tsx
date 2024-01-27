1918998b492f55d7fb8b14619968bfd815760791
Makstorm <mxm.horobets@gmail.com>
2024-01-27 18:05:31 +0200

    feat: new feature to backward icon, now has title

--- client/src/components/Navigations/BackwardArrow/index.tsx
+++ client/src/components/Navigations/BackwardArrow/index.tsx
@@ -1,22 +1,30 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import "./index.scss";
 import { INDEX_ROUTE } from "../../AppRouter/consts";
+import { FC } from "react";
 
-const BackArrow = () => {
+interface IBackArrowProps {
+  title?: string;
+}
+
+const BackArrow: FC<IBackArrowProps> = ({ title }) => {
   const location = useLocation();
   const navigate = useNavigate();
 
   return (
-    <button
-      className="back-arrow"
-      onClick={
-        location.pathname !== "/signin" && location.pathname !== "/signup"
-          ? () => navigate(-1)
-          : () => navigate(INDEX_ROUTE)
-      }
-    >
-      <img src="/svg/arrow-back.svg" alt="back-arrow" />
-    </button>
+    <div className="back-arrow">
+      <button
+        className="back-arrow__button"
+        onClick={
+          location.pathname !== "/signin" && location.pathname !== "/signup"
+            ? () => navigate(-1)
+            : () => navigate(INDEX_ROUTE)
+        }
+      >
+        <img src="/svg/arrow-back.svg" alt="back-arrow" />
+      </button>
+      {title && <h2 className="back-arrow__text">{title}</h2>}
+    </div>
   );
 };
 
